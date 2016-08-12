c724c1e88e83a733ff4541ab84c4e11e670f5b4a
peko <gasu.basu@gmail.com>
2016-08-12 12:02:45 +0300

    update

--- fonts/terminus-cli/build-powerline.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-../nafe-0.1/txt2psf ter-powerline.txt ter-powerline.psf
-psfaddtable ter-powerline.psf ter-powerline.map ter-powerline.psfu
--- fonts/terminus-cli/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo cp ter-powerline.psfu /usr/share/kbd/consolefonts/
-setfont ter-powerline
-
